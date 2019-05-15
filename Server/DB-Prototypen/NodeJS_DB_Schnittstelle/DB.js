3ac5dc183c52ca7f153ab6e10323755ffbbc89db
jackpearce98 <weather98@web.de>
2019-05-15 03:26:43 +0200

    Neuer Ansatz für die Datenbankanbindung

--- Server/DB-Prototypen/NodeJS_DB_Schnittstelle/DB.js
+++ Server/DB-Prototypen/NodeJS_DB_Schnittstelle/DB.js
@@ -1,65 +1,32 @@
-let mongoose=require("mongoose");
-const express=require("express");
-const app=express();
-const port=666;
-app.use(express.json());
+class DB {
+    constructor(database) {
+        this.mongoose=require("mongoose");
+        this.mongoose.connect('mongodb://localhost:27017/'+database, {useNewUrlParser: true});
+        this.db = this.mongoose.connection;
+        this.db.on('error', console.error.bind(console, 'connection error:'));
+        this.db.once('open', function () {
+            //Wenn es funktioniert
+            console.log("Datenbank verbunden");
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    //Wenn es funktioniert
-    console.log("Datenbank verbunden");
-});
+        });
+    }
 
-let postSchema= new mongoose.Schema({
-    title: String,
-    link: String,
-    text: String,
-    post_date: String,
-    tags: [String],
+    setSchema(schema){
+        let currentSchema;
+        currentSchema = new this.mongoose.Schema(schema);
+        this.post = this.mongoose.model('posts', currentSchema);
+        console.log("Ich sehe klar und deutlich ein Schema vor mir.")
+    }
+    postData(jsonObject) {
+        this.post(jsonObject).save(function (err) {
+            if (err) return console.error(err);
+        });
+        console.log("Erfolgreich dem Mongo übergeben!")
+    };
 
-    author: String,
-    author_id:Number,
-
-    section_id: Number,
-    section: String
-});
-let post=new mongoose.model('post', postSchema);
-
-app.post("/mr", (req, res)=> {
-    console.log(JSON.stringify(req.body.title));
-    let getNewPost=new post({
-        title: req.body.title,
-        link: req.body.link,
-        text: req.body.text,
-        post_date: Date,
-        tags: req.body.tags,
-
-        author: req.body.author,
-        author_id:req.body.id,
-
-        section_id: req.body.section_id,
-        section: req.body.section
-    });
-    getNewPost.save(function (err) {
-        if (err) return console.error(err);
-    });
-    console.log("Es funzt");
-    res.send("OK");
-});
-
-app.get("/mr", function(req, res){
-    post.findById(req.body.id,function (err, SelectedInfos) {
-             if(err!=null) {
-                 console.log(err);
-                 res.send(err);
-             }
-             else
-                 res.send(SelectedInfos);
-    });
-
-});
-
-app.listen(port,()=> console.log("Server hört Port auf Port: "+port+" zu"));
-
+    selectData(){
+        //Ohne setSchema(schema) geht das nicht!!!
+        return this.post.find;
+    }
+}
+module.exports.DB = DB;
