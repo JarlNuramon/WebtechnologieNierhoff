f4d0d01356d791a1e75bbc06ef9b2d2d0d0fb9e0
mkiller1911 <accmail@mkiller1911.de>
2019-06-05 12:52:15 +0200

    Added FavMan.js. Added an API to add Favorite Posts. Changed .gitignore. Added FavoriteDB to DB_Connection_Storage.js. Integraded FavMan.js to server.js.

---
+++ Server/server/modules/FavMan.js
@@ -0,0 +1,48 @@
+/*
+Diese Datei stellt folgende REST api's zur verfügung:
+
+    POST /api/favorite
+        Input Parameter:
+            post_id -> ObjectID
+            user -> String
+            token -> String
+
+        Return:
+            Jep or Nope
+
+*/
+const ff = require('./FunnyFunctions')
+const logger = require('./Logger')
+const FavoriteDB = require('../DB_Module/DB_Connection_Storage').FavoriteDB
+
+module.exports = app => {
+    app.post("/api/favorite", (req, res) => {
+        if(req.body.post_id !== undefined && req.body.user !== undefined && req.body.token !== undefined) {
+            ff.validateSession(req.body.user, req.body.token).then(user => {
+                if(user) {
+                    if(ff.checkObjectIdFormat(req.body.post_id)) {
+                        FavoriteDB.selectData({user: user.name}).then(favs => {
+                            if(favs.length == 1) {
+                                let newFavs = JSON.parse(JSON.stringify(favs[0]))
+                                newFavs.fav.push(req.body.post_id)
+                                FavoriteDB.updateData(favs[0], newFavs)
+                                res.send("Jep")
+                            } else {
+                                FavoriteDB.postData({user: user.name, fav: [req.body.post_id]})
+                                res.send("Jep")
+                            }
+                        })
+                    } else {
+                        res.send("Nope")
+                        logger.sendDebug("[FAVMAN][POST /api/favorite] FAILD invalid post_id.")
+                    }
+                } else {
+                    res.send("Nope")
+                    logger.sendDebug("[FAVMAN][POST /api/favorite] FAILD because Invalid user/token.")
+                }
+            })
+        } else {
+            logger.sendDebug("[FAVMAN][POST /api/favorite] called without required parameters.")
+        }
+    })
+}
