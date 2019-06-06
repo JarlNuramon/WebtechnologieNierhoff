75c8cdd48570e26a0d75b5ea198d41febc680ea1
JarlNuramon <saiedsadeghz@gmail.com>
2019-06-06 14:37:47 +0200

    Modals look nicer
    fixed the width and added scroll to text

--- Client/src/Thread.jsx
+++ Client/src/Thread.jsx
@@ -11,6 +11,12 @@ const customStyles = {
     marginRight: "-50%",
     "background-color": "#212121",
     transform: "translate(-50%, -50%)",
+    color: "white",
+    width: "50vw"
+  },
+  text: {
+    height: "100px",
+    "overflow-y": "auto",
     color: "white"
   }
 };
@@ -56,7 +62,7 @@ export class Thread extends React.Component {
             opts={this.opts}
             onReady={this._onReady}
           />
-          {this.post["text"]}
+          <div style={customStyles.text}>{this.post["text"]} </div>
         </ReactModal>
       </div>
     );
