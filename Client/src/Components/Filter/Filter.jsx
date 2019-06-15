5ef8695467fc9c6172ea7f79f39cd13dacdc725b
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2019-06-15 18:33:54 +0200

    Filter now uses api call

--- Client/src/Components/Filter/Filter.jsx
+++ Client/src/Components/Filter/Filter.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import { FilterItem } from "./ FilterItem";
 import "./Filter.css";
 import "axios";
-import { section } from "../../server";
+import { filteredData } from "../../server";
+
+const axios = require("axios");
 
 export class Filter extends React.Component {
   constructor(props) {
     super(props);
-
     this.searchAction = props.searchAction;
-    //TODO integration get all Sections
-    const axios = require("axios");
-    axios.get(section).then(response => this.filterItems(response.data));
+    this.getPost();
+  }
 
+  async getPost() {
+    await axios.get(filteredData).then( response => { this.t = (response.data[0]) } );
+    await this.filterItem();
+  }
+
+  filterItem() {
+    console.log(this.t);
     let json = require("./../../data.json");
-    this.t = json.Filter;
+    //this.t = json.Filter;
 
     this.datum = ["Letzte Stunde", "Heute", "Diese Woche", "Dieses Jahr"];
 
@@ -55,81 +62,6 @@ export class Filter extends React.Component {
         </tr>
       );
     }
-  }
-
-  filterItems(json) {
-    //json = JSON.stringify(json);
-    console.log(json[0]._id);
-    this.datum = ["Letzte Stunde", "Heute", "Diese Woche", "Dieses Jahr"];
-
-    this.max = Math.max(
-      this.datum.length,
-      json.length,
-      this.t.Fach.length,
-      this.t.Kurs.length
-    );
-
-    for (var i = 0; i < this.max; i++) {
-      this.menue.push(
-        <tr>
-          <FilterItem name={this.datum[i]} searchAction={this.searchAction} />
-          <FilterItem
-            name={this.fachbereich[i].name}
-            searchAction={this.searchAction}
-          />
-          <FilterItem
-            name={this.fach[i].name}
-            searchAction={this.searchAction}
-          />
-          <FilterItem
-            name={this.kurs[i].name}
-            searchAction={this.searchAction}
-          />
-        </tr>
-      );
-    }
-    /*
-
-
-    this.max = Math.max(
-        this.datum.length,
-        this.t.Fachbereich.length,
-        this.t.Fach.length,
-        this.t.Kurs.length
-    );
-    this.fachbereich = this.fill(this.t.Fachbereich);
-    this.fach = this.fill(this.t.Fach);
-    this.kurs = this.fill(this.t.Kurs);
-    this.state = {
-      checked: true,
-      datum: this.datum,
-      fach: this.fach,
-      kurs: this.kurs,
-      fachbereich: this.fachbereich
-    };
-
-
-    this.menue = [];
-    for (var i = 0; i < this.max; i++) {
-      this.menue.push(
-          <tr>
-            <FilterItem name={this.datum[i]} searchAction={this.searchAction} />
-            <FilterItem
-                name={this.fachbereich[i].name}
-                searchAction={this.searchAction}
-            />
-            <FilterItem
-                name={this.fach[i].name}
-                searchAction={this.searchAction}
-            />
-            <FilterItem
-                name={this.kurs[i].name}
-                searchAction={this.searchAction}
-            />
-          </tr>
-      );
-    }
-   */
   }
 
   fill(a) {
